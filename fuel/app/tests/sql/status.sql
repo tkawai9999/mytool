bbd0727bd541f5d32ee34ce6bd12531fca6677f4
tkawai <tkawai9999@gmail.com>
2015-12-12 23:22:57 +0900

    test sql modify

--- fuel/app/tests/sql/status.sql
+++ fuel/app/tests/sql/status.sql
@@ -1,9 +1,9 @@
 delete from statuses;
 alter table statuses auto_increment=1;
-insert into statuses(name,sort_no,delf) values 
-('未',1,0),
-('対応中',2,0),
-('保留',3,0),
-('完了',4,0),
-('ゴミ',5,1);
+insert into statuses(name,sort_no,delf,created_at,updated_at) values 
+('未',1,0,now(),now()),
+('対応中',2,0,now(),now()),
+('保留',3,0,now(),now()),
+('完了',4,0,now(),now()),
+('ゴミ',5,1,now(),now());
 
