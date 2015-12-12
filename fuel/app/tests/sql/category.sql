8acd7774b0277dd2db6e6e517e6a011ceae35f1d
tkawai <tkawai9999@gmail.com>
2015-12-12 23:35:32 +0900

    test sql modify

--- fuel/app/tests/sql/category.sql
+++ fuel/app/tests/sql/category.sql
@@ -3,6 +3,6 @@ alter table categories auto_increment=1;
 insert into categories(name,sort_no,delf,created_at,updated_at) values 
 ('category3',3,0,now(),now()),
 ('category1',1,0,now(),now()),
-('category1',2,0,now(),now());
+('category2',2,0,now(),now()),
 ('category4',4,1,now(),now());
 
