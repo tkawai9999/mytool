121ce4db1fab8ab07e57e59d20a8a89d2724393e
tkawai <tkawai9999@gmail.com>
2016-01-15 21:36:56 +0900

    gomi deleted

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -2,6 +2,4 @@
 return array(
 	'_root_'  => 'todolist/index',  // The default route
 	'_404_'   => 'todolist/404',    // The main 404 route
-	
-	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
 );
